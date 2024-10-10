3f934b83f30fb78ea15c4cd94771d35fe08db924
Rebeca Ferreira <rvsfrebeca@gmail.com>
2024-10-09 21:33:16 -0300

    initial commit to add maven and spring configurations

---
+++ src/main/java/franskestein/franskestein/FranskesteinApplication.java
@@ -0,0 +1,13 @@
+package franskestein.franskestein;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FranskesteinApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FranskesteinApplication.class, args);
+	}
+
+}
---
+++ src/test/java/franskestein/franskestein/FranskesteinApplicationTests.java
@@ -0,0 +1,13 @@
+package franskestein.franskestein;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FranskesteinApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
